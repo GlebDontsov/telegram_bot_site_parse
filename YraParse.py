433d088af9e4c81943870bea15cca63075f54a01
GlebDontsov <103101200+GlebDontsov@users.noreply.github.com>
2022-05-14 14:22:18 +0300

    Update YraParse.py

--- YraParse.py
+++ YraParse.py
@@ -1,112 +1,116 @@
-import random
+import logging
 import time
 
+import aiohttp
 import requests
-import telebot
-from bs4 import BeautifulSoup as bs
+from aiogram import Bot, Dispatcher, types, executor
 
-api_url = "https://wax.api.atomicassets.io/atomicassets/v1/collections"
+url = "https://qzlsklfacc.medianetwork.cloud/get_collections"
 
-bot = telebot.TeleBot("Token")
+time_last_collection = time.time()
+t = 0
+start_flag = True
+users = list()
 
-time_create_last = 0
-users = {}
+bot = Bot(token="your_token")
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
 
-
-# создание пользователя
-class User:
-    def __init__(self, chat_id, first_name, last_name):
-        self.chat_id = chat_id
-        self.first_name = first_name
-        self.last_name = last_name
+print("Все подключения и отключения в течении 1 минуты")
 
 
-# команда запуска бота
-@bot.message_handler(commands=["start"])
-def start(message):
-    users["{0}".format(message.chat.id)] = User(message.chat.id, message.from_user.first_name,
-                                                message.from_user.last_name)
-    first_name = users["{0}".format(message.chat.id)].first_name
-    bot.send_message(users["{0}".format(message.chat.id)].chat_id, f"Ok, {first_name}!")
-    parse(message)
+# получение данных о коллекциях
+async def getting_data():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
 
 
-# команда остановки бота
-@bot.message_handler(commands=["stop"])
-def stop(message):
-    bot.send_message(users["{0}".format(message.chat.id)].chat_id, f"Понял!")
-    if "{0}".format(message.chat.id) in users:
-        del users["{0}".format(message.chat.id)]
-
-
-def get_free_proxies():
-    url = "https://free-proxy-list.net/"
-    # получаем ответ HTTP и создаем объект soup
-    soup = bs(requests.get(url).content, "html.parser")
+# обработка полученных коллекций
+async def data_processing():
+    global t, time_last_collection
     try:
-        proxies = soup.find("textarea").text.split("\n")[3:]
-        return proxies[random.randint(1, len(proxies) - 1)]
-    except:
-        time.sleep(10)
-        get_free_proxies()
+        response = await getting_data()
+        # print("\033[37m {}".format("Отправка запросов на коллекции..."))
+    except Exception:
+        print("Ошибка в запросе на коллекции, возможно через минуты соединение востановится, но лучше перезапустить бота)...")
+        time.sleep(60)
+        response = await getting_data()
 
-
-# отправка сообщений пользователю
-def messages(collection_name, url, img, url_website):
-    if img != None:
-        img_data = requests.get(f"https://resizer.atomichub.io/images/v1/preview?ipfs={img}&size=370",
-                                headers=HEADERS).content
-
-        with open("img.webp", "wb") as f:
-            f.write(img_data)
-
-        for key in users:
-            bot.send_photo(users[key].chat_id, open("img.webp", "rb"))
-            bot.send_message(users[key].chat_id, f"Имя коллекции: {collection_name}\n"
-                                                 f"Ссылка на коллекцию: {url}\nСсылка на сайт: {url_website}")
-
-
-# поиск новых коллекций на сайте
-def parse(message):
-    while True:
-        global HEADERS
-        proxy = get_free_proxies().split(':')[0]
-        HEADERS = {
-            "Referer": "https://wax.atomichub.io",
-            "User-Agent": f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          f"Chrome/{proxy} YaBrowser/22.1.5.810 Yowser/2.5 Safari/537.36",
-            "sec-ch-ua-platform": "Windows"
-        }
-
-        try:
-            res = requests.get(api_url, headers=HEADERS)
-        except:
-            time.sleep(60)
-            parse()
-
-        items = res.json()
-
-        if res.status_code == 200:
-            for item in items["data"]:
-                global time_create_last
-                created_at_time = int(item["created_at_time"][:-3])
-                collection_name, img = item["collection_name"], item["img"]
-
-                if time_create_last < created_at_time:
-                    time_create_last = created_at_time
-                    url = f"https://wax.atomichub.io/explorer/collection/{collection_name}"
-
-                    if "url" in item["data"]:
-                        url_website = item["data"]["url"]
-                    else:
-                        url_website = "Cсылки на сайт нет"
-
-                    messages(collection_name, url, img, url_website)
-
+    for item in response:
+        if item["updateAuth"] == "[]":
+            continue
         else:
-            bot.send_message(users["{0}".format(message.chat.id)].chat_id, "Error!")
+            if item["date"] > time_last_collection:
+                create_time = item["date"]
 
-        time.sleep(5)
+                if create_time > t:
+                    t = create_time
+
+                website_collection = item["website"]
+                name_collection = item["name"]
+                img_collection = item["img"]
+                img_preview_collection = item["imgpreview"]
+
+                saving_an_image(img_preview_collection, img_collection)
+                await sending_messages(name_collection, website_collection)
+            else:
+                break
+
+    if t > time_last_collection:
+        time_last_collection = t
+
+    time.sleep(60)
+    await data_processing()
 
 
-bot.polling(none_stop=True)
+# сохранение фотографий
+def saving_an_image(img_preview, img):
+    if img_preview:
+        img_preview = requests.get(img_preview).content
+        with open("img.webp", "wb") as f:
+            f.write(img_preview)
+
+    if img:
+        img = requests.get(img).content
+        with open("img1.webp", "wb") as f:
+            f.write(img)
+
+
+# отправка сообщений
+async def sending_messages(name, website):
+    if users:
+        print("Отправка сообщений пользователям")
+        for user in users:
+            await bot.send_photo(chat_id=user, photo=open("img.webp", "rb"))
+            await bot.send_photo(chat_id=user, photo=open("img1.webp", "rb"))
+            await bot.send_message(chat_id=user, text=f"Название коллекции: {name}\n"
+                                                      f"Сайт коллекции: {website}\n")
+
+
+# команда запуска Бота
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    global start_flag
+    if message.chat.id not in users:
+        users.append(message.chat.id)
+        await bot.send_message(chat_id=message.chat.id, text=f"Привет {message.chat.first_name}, работаем!")
+        print(f"Подключился пользователь {message.chat.first_name}...")
+
+    if start_flag:
+        start_flag = False
+        await data_processing()
+
+
+# команда остановки Бота
+@dp.message_handler(commands=["stop"])
+async def stop(message: types.Message):
+    if message.chat.id in users:
+        print(f"пользователь {message.chat.first_name} отключился...")
+        users.remove(message.chat.id)
+        await bot.send_message(chat_id=message.chat.id, text=f"Вы отключены")
+    else:
+        await bot.send_message(chat_id=message.chat.id, text=f"Вы не были подключены)")
+
+
+executor.start_polling(dp, skip_updates=True, timeout=600)
